46bdf8f218bd024b09f2dc7d6cd92cfc0fccc38a
24LP4066 24LP4066 <24LP4066@G303-CC0144.local>
2025-01-24 12:33:05 +0800

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+const timer = document.getElementById("timer");
+const stopButton = document.getElementById("stop");
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset")
+let timerInterval;
+let startTime = 0;
+let test = setInterval(() => {
+    console.log("test")
+}, 10000);
+console.log(test);
